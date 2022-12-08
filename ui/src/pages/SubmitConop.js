77ca1ed7b3ecef9b567ffb8846d2e755b03d240b
Justin <levenebb@gmail.com>
2022-12-08 14:57:36 -0500

    no idea

--- ui/src/pages/SubmitConop.js
+++ ui/src/pages/SubmitConop.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const SubmitConop = () => {
-  return <div>SubmitConop</div>;
+  const [submitFormData, setSubmitFormData] = useState({});
+  const [validated, setValidated] = useState(false);
+
+  //Set state for the form fields
+  const handleFormData = (event) => {
+    let newData = { ...submitFormData };
+    newData[event.target.id] = event.target.value;
+    setSubmitFormData(newData);
+  };
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  return (
+    <div className="submit-form-container">
+      <h1>Submit CONOP</h1>
+      <Container>
+        <Row>
+          <Col>1 of 2</Col>
+          <Col>2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default SubmitConop;
