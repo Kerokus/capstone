7b2700e14f97f926bdcbf01fb5795f985fd6aab2
Jeff <mynameisnoland@gmail.com>
2022-12-08 14:35:49 -0500

    Basic GET, DELETE, POST, and PUT endpoints complete

--- api/src/app.js
+++ api/src/app.js
@@ -205,5 +205,78 @@ app.post('/teams', async (req, res) => {
     res.status(400).send(`Post failed`);
   }
 })
+/////////////////////////////////////////////////////////////  PUT  ////////////////////////////////////////////////////////////
+//missions update endpoint
+app.put('/missions/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    let updatedMission = {
+      start_date: req.body.start_date,
+      end_date: req.body.end_date,
+      location: req.body.location,
+      name: req.body.name,
+      description: req.body.description,
+      status: req.body.status,
+      purpose: req.body.purpose,
+      authority: req.body.authority,
+      end_state: req.body.end_state,
+      transportation_methods: req.body.transportation_methods,
+      timeline: req.body.timeline,
+      pace: req.body.pace,
+      risks: req.body.risks,
+      decision_point: req.body.decision_point,
+      team_id: req.body.team_id,
+    }
+    await knex('missions').where('id', id).update(updatedMission);
+    res.status(201).send('Mission successfully updated.')
+  } catch(e) {
+    console.log(e);
+    res.status(400).send(`Update failed`);
+  }
+})
+
+//personnel update endpoint
+app.put('/personnel/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    let updatedPersonnel = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      rank: req.body.rank,
+      mos: req.body.mos,
+      email: req.body.email,
+      status: req.body.status,
+      location: req.body.location,
+      deployment_start: req.body.deployment_start,
+      deployment_end: req.body.deployment_end,
+      team_id: req.body.team_id,
+    }
+    await knex('personnel').where('id', id).update(updatedPersonnel);
+    res.status(201).send('Personnel successfully updated.')
+  } catch(e) {
+    console.log(e);
+    res.status(400).send(`Update failed`);
+  }
+})
+
+//teams update endpoint
+app.put('/teams/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    let updatedTeams = {
+      name: req.body.name,
+      location: req.body.location,
+      comms_status: req.body.comms_status,
+      personnel_status: req.body.personnel_status,
+      equipment_status: req.body.equipment_status,
+    }
+    await knex('teams').where('id', id).update(updatedTeams);
+    res.status(201).send('Team successfully updated.')
+  } catch(e) {
+    console.log(e);
+    res.status(400).send(`Update failed`);
+  }
+})
+
 
 module.exports = app;
