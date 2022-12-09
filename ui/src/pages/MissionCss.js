90ae8912b6310fb7d52712b4a02571ecb42d7773
katrinalhill <112709630+katrinalhill@users.noreply.github.com>
2022-12-09 18:04:07 -0500

    Add files via upload

---
+++ ui/src/pages/MissionCss.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const MissionCss = () => {
+  return (
+    <div>
+      <div class="missionsoverview-container">
+   
+    <div class="mission-map-1">mission-map-1</div>
+    <div class="mission-map-2">mission-map-2</div>
+    <div class="mission-map-3">mission-map-3</div>
+    <div class="mission-map-4">mission-map-4</div>
+    <div class="mission-map-5">mission-map-5</div>
+    <div class="mission-map-1-team">
+      <h1>Name:</h1>
+      <h1>Team:</h1>
+      <h1>Start Date:</h1>
+    </div>
+    <div class="mission-map-2-team">
+    <h1>Name:</h1>
+      <h1>Team:</h1>
+      <h1>Start Date:</h1>
+    </div>
+    <div class="mission-map-3-team">
+    <h1>Name:</h1>
+      <h1>Team:</h1>
+      <h1>Start Date:</h1>
+      <h1>.</h1>
+      <h1>.</h1>
+      <h1>.</h1>
+  
+    </div>
+    <div class="mission-map-4-team">
+    <h1>Name:</h1>
+      <h1>Team:</h1>
+      <h1>Start Date:</h1>
+    </div>
+    <div class="mission-map-5-team">
+    <h1>Name:</h1>
+      <h1>Team:</h1>
+      <h1>Start Date:</h1>
+    </div>
+  </div>
+  </div>
+  )
+}
+
+export default MissionCss
---
+++ ui/src/pages/TeamsCss.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const TeamsCss = () => {
+  return (
+ <div>
+    <div class="teamsoverview-container">
+ 
+  <div class="teams-map-1">teams-map-1</div>
+  <div class="teams-map-2">teams-map-2</div>
+  <div class="teams-map-3">teams-map-3</div>
+  <div class="teams-map-4">teams-map-4</div>
+  <div class="teams-map-5">teams-map-5</div>
+  <div class="teams-map-1-team">
+    <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+  </div>
+  <div class="teams-map-2-team">
+  <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+  </div>
+  <div class="teams-map-3-team">
+  <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+
+  </div>
+  <div class="teams-map-4-team">
+  <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+  </div>
+  <div class="teams-map-5-team">
+  <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+  </div>
+  <div class="unassigned-teams">unassigned-teams</div>
+</div>
+</div>
+  )
+}
+
+export default TeamsCss
