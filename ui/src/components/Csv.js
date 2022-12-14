09e809074bc514be57288a4724587fc483a42f17
Jeff <mynameisnoland@gmail.com>
2022-12-14 14:11:59 -0500

    asdf

--- ui/src/components/Csv.js
+++ ui/src/components/Csv.js
@@ -1,74 +1,74 @@
-// import React, { useCallback, useMemo, useRef, useState, useContext} from 'react';
-// import { ContextProvider, GlobalContext } from "../Context/GlobalContext";
-// import { CSVLink } from "react-csv";
-// import DownloadIcon from '@mui/icons-material/Download';
+import React, { useCallback, useMemo, useRef, useState, useContext} from 'react';
+import { ContextProvider, GlobalContext } from "../Context/GlobalContext";
+import { CSVLink } from "react-csv";
+import DownloadIcon from '@mui/icons-material/Download';
 
-// import { render } from 'react-dom';
-// import { AgGridReact } from 'ag-grid-react';
-// import 'ag-grid-community/styles/ag-grid.css';
-// import 'ag-grid-community/styles/ag-theme-alpine.css';
-
-
-
-
-// const Csv = () => {
-//   const ctx = useContext(GlobalContext);
-//   // const [personnelData, setPersonnelData] = useState({})
-//   // console.log(ctx.personnelData)
-
-//   const gridRef = useRef();
-//   const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
-//   const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 
 
-//   const defaultColDef = useMemo(() => {
-//     return {
-//       editable: true,
-//       resizable: true,
-//       minWidth: 100,
-//       flex: 1,
-//     };
-//   }, []);
-//   const popupParent = useMemo(() => {
-//     return document.body;
-//   }, []);
 
-//   const [columnDefs, setColumnDefs] = useState([
-//     { field: 'id' },
-//     { field: 'first_name' },
-//     { field: 'last_name' },
-//     { field: 'rank' },
-//     { field: 'mos' },
-//     { field: 'email' },
-//     { field: 'status' },
-//     { field:  "team_name"},
-//     { field:  "location"},
-//   ]);
+const Csv = () => {
+  const ctx = useContext(GlobalContext);
+  // const [personnelData, setPersonnelData] = useState({})
+  // console.log(ctx.personnelData)
 
-//   const onBtnExport = useCallback(() => {
-//     gridRef.current.api.exportDataAsCsv();
-//   }, []);
-
-//   const onBtnUpdate = useCallback(() => {
-//     document.querySelector(
-//       '#csvResult'
-//     ).value = gridRef.current.api.getDataAsCsv();
-//   }, []);
-
-//   console.log("search term", ctx.searchTerm)
+  const gridRef = useRef();
+  const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+  const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
 
 
-//   return (
-//     <div >
-//       <div>
-//         <div>
+
+  const defaultColDef = useMemo(() => {
+    return {
+      editable: true,
+      resizable: true,
+      minWidth: 100,
+      flex: 1,
+    };
+  }, []);
+  const popupParent = useMemo(() => {
+    return document.body;
+  }, []);
+
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'id' },
+    { field: 'first_name' },
+    { field: 'last_name' },
+    { field: 'rank' },
+    { field: 'mos' },
+    { field: 'email' },
+    { field: 'status' },
+    { field:  "team_name"},
+    { field:  "location"},
+  ]);
+
+  const onBtnExport = useCallback(() => {
+    gridRef.current.api.exportDataAsCsv();
+  }, []);
+
+  const onBtnUpdate = useCallback(() => {
+    document.querySelector(
+      '#csvResult'
+    ).value = gridRef.current.api.getDataAsCsv();
+  }, []);
+
+  console.log("search term", ctx.searchTerm)
+
+
+  return (
+    <div >
+      <div>
+        <div>
          
-//           <DownloadIcon className="download-button" onClick={onBtnExport}>Download Personnel</DownloadIcon >
-//         </div>
-//         <div>
-//           <div>
-//             <div>
+          <DownloadIcon className="download-button" onClick={onBtnExport}>Download Personnel</DownloadIcon >
+        </div>
+        <div>
+          <div>
+            <div>
               
               <AgGridReact
                 ref={gridRef}
@@ -83,13 +83,13 @@
             </div>
           </div>
 
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+        </div>
+       </div>
+     </div>
+  );
+}
 
-// export default Csv
+export default Csv
 
 
 
