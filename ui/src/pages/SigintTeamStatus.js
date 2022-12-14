2264b31f16483258e73a6df96f142a104f22227a
Kerokus <94187151+Kerokus@users.noreply.github.com>
2022-12-14 13:13:28 -0500

    Revert "node modules deleted"

---
+++ ui/src/pages/SigintTeamStatus.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useContext } from "react";
+import { ContextProvider, GlobalContext } from "../Context/GlobalContext";
+
+const SigintTeamStatus = () => {
+  const ctx = useContext(GlobalContext);
+  
+  /* iterates over the teams state variable, then pushes all teams with 'SIGINT' in their name to the sigintTeamsArray,
+  then sets the sigintTeams state variable with that array. [fires whenever the teams state variable changes]. */
+  useEffect(() => {
+    let sigintTeamsArray = [];
+    for (let i = 0; i < ctx.teams.length; i++){
+      let team_name = (ctx.teams[i].team_name).toUpperCase()
+      if (team_name.includes('SIGINT')) {
+        sigintTeamsArray.push(ctx.teams[i])
+      }
+      ctx.setSigintTeams(sigintTeamsArray)
+    }
+  }, [ctx.teams.length])
+
+  /* sorts teams into 1 of 3 arrays (greenSigintTeams, yellowSigintTeams, redSigintTeams) depending on their 
+  comms, personnel, and equipment status. If a team has ANY red status they are pushed into the red array,
+  if a team has no red and some yellow they are pushed into the yellow array, and if a team has no red AND no yellow status
+  they are pushed into the green array. the corresponding state variables are then set using those arrays. 
+  [fires everytime the sigintTeams state variable changes]. */
+  useEffect(() => {
+    let greenSigintTeams = [];
+    let yellowSigintTeams = [];
+    let redSigintTeams = [];
+      for(let i = 0; i <= ctx.sigintTeams.length - 1; i++) {
+        let comms_status = (ctx.sigintTeams[i].comms_status).toUpperCase()
+        let personnel_status = (ctx.sigintTeams[i].personnel_status).toUpperCase()
+        let equipment_status = (ctx.sigintTeams[i].equipment_status).toUpperCase()
+        if (comms_status === 'RED' || personnel_status === 'RED' || equipment_status === 'RED') {
+          redSigintTeams.push(ctx.sigintTeams[i].team_name)
+          ctx.setRedSigintTeams(redSigintTeams)
+        } else if (comms_status === 'YELLOW' || personnel_status === 'YELLOW' || equipment_status === 'YELLOW') {
+          yellowSigintTeams.push(ctx.sigintTeams[i].team_name)
+          ctx.setYellowSigintTeams(yellowSigintTeams)
+        } else {
+          greenSigintTeams.push(ctx.sigintTeams[i].team_name)
+          ctx.setGreenSigintTeams(greenSigintTeams)
+        }
+      }
+  }, [ctx.sigintTeams])
+
+  /* the return sets up 3 divs (red_teams, yellow_teams, green_teams), each div maps over the corresponding 
+  state variables (redSigintTeams, yellowSigintTeams, greenSigintTeams) and returns the team names. */
+  return (
+    <>
+    <div className="sigint-team-status">
+      <h3 className="team-status-header">SIGINT Teams Status:</h3>
+      <div className="red_teams">{ctx.redSigintTeams.map((team, index) => {
+        return <div key={index}>{team} 🟥</div>})} 
+      </div>
+      <div className="yellow_teams">{ctx.yellowSigintTeams.map((team, index) => {
+        return <div key={index}>{team} 🟨</div>})} 
+      </div>
+      <div className="green_teams">{ctx.greenSigintTeams.map((team, index) => {
+        return <div key={index}>{team} 🟩</div>})} 
+      </div>
+    </div>
+    </>
+  )
+};
+
+export default SigintTeamStatus;
---
+++ ui/src/pages/TeamsCss.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const TeamsCss = () => {
+  return (
+ <div>
+    <div className="teamsoverview-container">
+ 
+  <div className="teams-map-1">teams-map-1</div>
+  <div className="teams-map-2">teams-map-2</div>
+  <div className="teams-map-3">teams-map-3</div>
+  <div className="teams-map-4">teams-map-4</div>
+  <div className="teams-map-5">teams-map-5</div>
+  <div className="teams-map-1-team">
+    <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+  </div>
+  <div className="teams-map-2-team">
+  <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+  </div>
+  <div className="teams-map-3-team">
+  <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+    <h1>.</h1>
+
+  </div>
+  <div className="teams-map-4-team">
+  <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+  </div>
+  <div className="teams-map-5-team">
+  <h1>Name:</h1>
+    <h1>Team:</h1>
+    <h1>Start Date:</h1>
+  </div>
+  <div className="unassigned-teams">unassigned-teams</div>
+</div>
+</div>
+  )
+}
+
+export default TeamsCss
