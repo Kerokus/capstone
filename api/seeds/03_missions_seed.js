f98ebbf42bc036e1addabf6d4c6355dd3bb4289c
riyaad.mohamed <riyaad.mohamed.mil@army.mil>
2022-12-16 11:27:45 -0500

    push

--- api/seeds/03_missions_seed.js
+++ api/seeds/03_missions_seed.js
@@ -44,13 +44,8 @@ exports.seed = async function (knex) {
     },
     {
       id: 2,
-<<<<<<< HEAD
       start_date: "2022-12-17",
       end_date: "2022-12-26",
-=======
-      start_date: "2022-12-16",
-      end_date: "2022-12-15",
->>>>>>> main
       location: {
         city_base: "Camp Arifjan",
         country: "Kuwait",
